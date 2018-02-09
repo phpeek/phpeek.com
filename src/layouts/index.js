a309980cbd912826b8fb087aa8a12e4be67ae6a9
Paweł Małolepszy <pawel.malolepszy@gmail.com>
2018-02-09 23:13:42 +0100

    Removes boilerplate code.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-import Header from '../components/Header'
 import './index.css'
 
 const TemplateWrapper = ({ children }) => (
@@ -14,7 +13,6 @@ const TemplateWrapper = ({ children }) => (
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-    <Header />
     <div
       style={{
         margin: '0 auto',
