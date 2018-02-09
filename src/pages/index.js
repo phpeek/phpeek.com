9003e730b4fdf6c23443a91a8159951964bf5803
Paweł Małolepszy <pawel.malolepszy@gmail.com>
2018-02-10 00:52:38 +0100

    Adds basic personal info.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 const IndexPage = () => (
   <div>
+    <h1>Paweł Małolepszy</h1>
+    <h2>Software Engineer @ <a href="https://www.pracujwunii.pl">Pracuj w Unii</a></h2>
   </div>
 )
 
